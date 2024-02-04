e511b0dd7a2173ec694ae8995becb23e197a73a8
michael <ydna.mjs@gmail.com>
2024-02-04 14:22:56 -0500

    Created basic bot that can log in

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,1 +1,14 @@
-console.log("Hello World");
+import {Client, Events, GatewayIntentBits} from "discord.js";
+import {BOT_TOKEN} from "../config";
+
+function main() {
+	const bot = new Client({intents: [GatewayIntentBits.Guilds]});
+
+	bot.once(Events.ClientReady, (readyClient) => {
+		console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	});
+
+	bot.login(BOT_TOKEN);
+}
+
+main();
